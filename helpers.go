2a72c09ffdd78787f8a56651b7e499d4ba21ef8d
Jeroen Hulshof <jeroenhulshof@mbp-van-jeroen.kpn>
2024-03-31 17:57:23 +0200

    created helpers

---
+++ helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func newGetRequest(apiEndpoint string) (string, error) {
+	response, error := http.Get(apiEndpoint)
+
+	if error != nil {
+		log.Fatal("Error making GET request:", error)
+	}
+
+	defer response.Body.Close()
+
+	body, error := io.ReadAll(response.Body)
+
+	if error != nil {
+		log.Fatal("Error reading response body:", error)
+	}
+
+	return string(body), nil
+}
